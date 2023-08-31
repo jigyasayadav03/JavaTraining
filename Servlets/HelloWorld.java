f4eadddeb93b2045871fb783fd44461a2a69178e
jigyasa yadav <jigyasa.yadav@brainvire.com>
2023-08-31 19:07:28 +0530

    Servlets tutorials

---
+++ Servlets/HelloWorld.java
@@ -0,0 +1,38 @@
+package com.jigyasa.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.print("<HTML><BODY>");
+		out.print("<P>HELLO!!</P>");
+		out.print("</body></html>");
+		
+	}
+}
+
