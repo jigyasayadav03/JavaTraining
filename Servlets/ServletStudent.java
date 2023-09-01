ff391d9461ffb7eb08ec5841c2a56236cced8eaa
jigyasa yadav <jigyasa.yadav@brainvire.com>
2023-09-01 19:48:28 +0530

    servlet jsp

---
+++ Servlets/ServletStudent.java
@@ -0,0 +1,43 @@
+package com.jigyasa.servlet;
+import com.jigyasa.*;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Servlet implementation class ServletStudent
+ */
+public class ServletStudent extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletStudent() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method
+List<StudentServlets> students = StudentServlet.getStudents();
+		// step 2: add students to request object
+		request.setAttribute("student_list", students);
+		// step 3: get request dispatcher
+		RequestDispatcher dispatcher = 
+				request.getRequestDispatcher("students_two.jsp");		
+		// step 4: now forward to JSP
+		dispatcher.forward(request, response);
+	}
+
+}
+
+
--- Servlets/formServlet.java
+++ Servlets/formServlet.java
@@ -11,7 +11,6 @@ import java.io.PrintWriter;
 /**
  * Servlet implementation class formServlet
  */
-@WebServlet("/formServlet")
 public class formServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
